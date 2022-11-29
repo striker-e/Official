3a62e03d4ae1c58f92fe9f777320689c3bb425ee
striker-e <ephialtesstriderhs@gmail.com>
2022-11-29 20:59:14 +0000

    Fixed Laser Alignment and Vector Azimuthal Angle Problem

--- laser.py
+++ laser.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 #add height as a constructor parameter
 class Laser(pygame.sprite.Sprite):
     def __init__(self,angle,speed,spawnpoint):
@@ -7,8 +8,11 @@ class Laser(pygame.sprite.Sprite):
         self.base_image = pygame.transform.rotate(self.base_image, 0)
         self.image = self.base_image
         self.rect = self.image.get_rect(center = spawnpoint)
+        self.pos = pygame.Vector2(self.rect.center) #Storing pos as a vector which handles floating point, which will fix rounding errors and bugs
         self.vec = pygame.Vector2()
-        self.vec.from_polar((speed,angle + 90))
+        self.angle = angle * -1
+        self.vec.x = math.sin(((self.angle)/180)*math.pi) * speed
+        self.vec.y = math.cos(((self.angle)/180)*math.pi) * speed
         oldcenter = self.rect.center
         self.image = pygame.transform.rotate(self.base_image, angle)
         self.rect = self.image.get_rect()
@@ -18,8 +22,9 @@ class Laser(pygame.sprite.Sprite):
         #if self.rect.y <= -50 or self.rect.y >= self.yconstraint + 50:
             #self.kill()
     def update(self):
-        self.rect.x += self.vec[0]
-        self.rect.y -= self.vec[1]
+        self.pos.x += self.vec.x
+        self.pos.y -= self.vec.y
+        self.rect.center = self.pos.x,self.pos.y
         #self.destroy()
         
 class AlienLaser(pygame.sprite.Sprite):
