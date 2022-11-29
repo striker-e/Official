92708abd986d2a6d102a89203e0f3f0381ecd967
striker-e <ephialtesstriderhs@gmail.com>
2022-11-29 11:09:37 +0000

    Added Comments

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from laser import Laser, AlienLaser
 from pausemenu import PauseMenu
 import random
 class Game:
+    #Game Class Constructor,Creates Gamescreen with the initial resolution and positions it correctly, creates only one player object. Creates aliens once.
     def __init__(self,width,height):
         self.width = width
         self.height = height
@@ -19,14 +20,14 @@ class Game:
         self.alien_direction = 1
         self.alien_lasers = pygame.sprite.Group()
         self.all_aliens = self.aliens.sprites()
-    def aliencreation(self, cols, rows):
+    def aliencreation(self, cols, rows): #Creates aliens row by row, with spacing relative to gamescreen's left border.
         x_dist = 30
         y_dist = 30
         for row_index, row in enumerate(range(rows)):
             for col_index, col in enumerate(range(cols)):
                 x = col_index * x_dist + self.gamescreen.gamewindow.left + 10
                 y = row_index * y_dist + self.gamescreen.gamewindow.top + 10
-                if row_index == 0:
+                if row_index == 0: #Changes alien sprites depending on rows.
                     alien_sprite = Alien('yellow', x, y)
                 elif 1 <= row_index <= 2: 
                     alien_sprite = Alien('green', x, y)
@@ -35,7 +36,7 @@ class Game:
                 else:
                     alien_sprite = Alien('yellow', x ,y)
                 self.aliens.add(alien_sprite)
-    def aliencheckpos(self,leftconstraint,rightconstraint):
+    def aliencheckpos(self,leftconstraint,rightconstraint): #Check if touching gamescreen's border and move them down.
         for alien in self.all_aliens:
             if alien.rect.right >= rightconstraint:
                 self.alien_direction = -1
@@ -45,16 +46,16 @@ class Game:
                 self.alien_direction = 1
                 self.alien_move_down(0.5)
                 print(alien.rect.x,alien.rect.y)
-    def alien_move_down(self, distance):
+    def alien_move_down(self, distance): #Moves aliens down.
         if self.aliens:
             for alien in self.aliens.sprites(): 
                 alien.pos[1] += distance
-    def alien_shoot(self):
+    def alien_shoot(self): #Aliens shoot lasers
         if self.aliens.sprites():
             random_alien = random.choice(self.aliens.sprites())
             laser_sprite = AlienLaser(random_alien.rect.center, height)
             self.alien_lasers.add(laser_sprite)
-    def run(self):
+    def run(self): #Run Object, draws gamescreen, player,alien each update using each of the sprite update and draw functions.
         self.gamescreen = GameScreen(displayscreen, self.width, self.height)
         self.player.sprite.update(self.gamescreen.gamewindow.left,self.gamescreen.gamewindow.right)
         self.aliens.update(self.alien_direction)
@@ -68,44 +69,44 @@ class Game:
         self.alien_lasers.draw(displayscreen)
         self.gamescreen.draw()
 if __name__ == "__main__":
-    pygame.init()
+    pygame.init() # Pygame Initialisation and base variable's definitions.
     width=1280
     height=720
     base_width = 1280
     base_height = 720
-    background = pygame.image.load('./assets/background.jpg')
+    background = pygame.image.load('./assets/background.jpg') #Loading the background images.
     background = pygame.transform.scale(background, (width, height))
-    screen = pygame.display.set_mode((width,height),pygame.RESIZABLE)
-    displayscreen = screen.copy()
+    screen = pygame.display.set_mode((width,height),pygame.RESIZABLE) #Making the main screen.
+    displayscreen = screen.copy() #Make a fakescreen to draw all objects onto so can resize this whole screen when a resize occurs.
     pygame.display.set_caption("PROJ")
     pygame.font.init()
-    mainmenu = Menu(width,height,screen)
+    mainmenu = Menu(width,height,screen) #Constructs objects for the main menu and the pause menu.
     pausemenu = PauseMenu(displayscreen,width,height)
     pausestate = False
     running = False
     clock = pygame.time.Clock()
     while True:
-        if running == False and pausestate == False:
+        if running == False and pausestate == False: #Creates a new game if in main menu.
             game = Game(base_width,base_height)
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
+        keys = pygame.key.get_pressed() #Gets key's pressed each iteration.
+        for event in pygame.event.get(): #Check events one by one
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.VIDEORESIZE:
+            elif event.type == pygame.VIDEORESIZE: #Upon resize, sets width and height but doesn't let window be smaller than 800 x 600 for better usability.
                 new_width = max(800,event.size[0])
                 new_height = max(600,event.size[1])
                 screen = pygame.display.set_mode((new_width,new_height),pygame.RESIZABLE)
                 width,height = screen.get_width(),screen.get_height()
-                #game.width, game.height = width,height
-            elif event.type == pygame.KEYDOWN:
+                #game.width, game.height = width,height, changed this as would make gamescreen be scaled twice.
+            elif event.type == pygame.KEYDOWN: #Key Checks
                 if event.key == pygame.K_k:
                     mainmenu.change(True)
                 elif event.key == pygame.K_i:
                     mainmenu.change(False)
                 elif event.key == pygame.K_RETURN and mainmenu.index == 0 and pausestate == False:
                     running = True
-                elif event.key == pygame.K_RETURN and pausestate == True:
+                elif event.key == pygame.K_RETURN and pausestate == True: #Pause Menu key checks
                     running = False
                     pausestate = False
                 elif event.key == pygame.K_BACKSPACE and pausestate == True:
@@ -113,17 +114,17 @@ if __name__ == "__main__":
                     pausestate = False
                 elif event.key == pygame.K_ESCAPE:
                     running = False
-                elif running and event.key == pygame.K_p:
+                elif running and event.key == pygame.K_p: #Draw pause menu.
                     pausestate = True
                     running = False
                     if pausestate:
                         pausemenu.draw()
-        if running and pausestate == False:
+        if running and pausestate == False: #Run game each frame.
             game.run()
-        screen.blit(pygame.transform.scale(displayscreen,(screen.get_width(),screen.get_height())),(0,0))
+        screen.blit(pygame.transform.scale(displayscreen,(screen.get_width(),screen.get_height())),(0,0)) #Scales fakesceen correctly.
         if running == False and pausestate == False:
-            background = pygame.transform.scale(background, (width, height))
+            background = pygame.transform.scale(background, (width, height)) #Scales the background correctly.
             screen.blit(background, (0,0))
-            mainmenu.draw()
-        pygame.display.update()
-        clock.tick(60)
+            mainmenu.draw() #Draws the menu.
+        pygame.display.update() #Updates all display changes
+        clock.tick(60) #Sets the FPS to 60
--- pausemenu.py
+++ pausemenu.py
@@ -1,24 +1,13 @@
 import pygame
 class PauseMenu():
-    def __init__(self,screen,width,height):
+    def __init__(self,screen,width,height): #Constructor, takes in the screen to draw, width and height for positioning info. Loads font.
         self.screen = screen
         self.font = pygame.font.Font('assets/font.TTF',16)
         self.width = width
         self.height = height
-    def draw(self):
+    def draw(self): #Draw function, fills screen black when paused and draws text over.
         self.screen.fill("black")
         text = self.font.render("To return to menu press return or To resume game press backspace", True, "white")
         textrect = text.get_rect()
-        textrect.center = self.width/2,self.height/2
+        textrect.center = self.width/2,self.height/2 # Center text correctly.
         self.screen.blit(text,textrect)
-    def keychecker(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:
-            print("case2")
-            return 2
-        elif keys[pygame.K_BACKSPACE]:
-            print("case 1")
-            return 1
-        else:
-            print("case 3")
-            return 3
